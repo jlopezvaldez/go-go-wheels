7ab2bffcc2d59af014fb13a4060902787d260b7a
jonathan <jlopezv0324@gmail.com>
2024-08-28 11:29:00 -0700

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321/',
   integrations: [tailwind()],
   output: "server",
   adapter: netlify()
