f95f6bfd8d4df4a853b9ea2f517a811f3aa5d6a5
jonathan <jlopezv0324@gmail.com>
2024-08-28 11:29:10 -0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321/',
+  site: 'https://gogowheelsandtires.com/',
   integrations: [tailwind()],
   output: "server",
   adapter: netlify()
