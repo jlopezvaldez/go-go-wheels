07c67f10b7ef217fc8599c2e8b955944135b0345
jonathan <jlopezv0324@gmail.com>
2024-08-28 12:55:04 -0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gogowheelsandtires.com/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
