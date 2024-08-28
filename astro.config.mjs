61abab2054a112d20c7460675fc00836afeb8370
jonathan <59666853+jlopezvaldez@users.noreply.github.com>
2024-08-28 11:30:36 -0700

    Feature/seo (#2)
    * commit
    
    * commit
    
    * commit
    
    * seo improvements
    
    * commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gogowheelsandtires.com/',
   integrations: [tailwind()],
   output: "server",
   adapter: netlify()
